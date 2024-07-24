8f98a0448e311141ccf4705c507c85f93b8cc0e7
FacuHdezz <facuhernandez2000@gmail.com>
2024-07-24 14:43:35 -0300

    agregando iconos al aside

--- src/components/Aside.jsx
+++ src/components/Aside.jsx
@@ -1,26 +1,31 @@
 import { Link } from 'react-router-dom';
 import Map from '../assets/imagenes/mapa-1.webp';
+import Home from '../assets/iconos/home.svg'
+import Info from '../assets/iconos/info.svg'
+import Products from '../assets/iconos/products.svg'
+import Location from '../assets/iconos/location.svg'
 
 const Aside = () => {
     return (
         <aside className="col-span-1 max-lg:hidden">
             <div className=' bg-white my-3 ml-3 rounded-lg p-4 flex flex-col gap-1'>
-                <article className="text-base 2xl:text-lg flex flex-col divide-y divide-gray-400">
-                    <Link to={"/"} className="hover:bg-gray-300"><h1 className="text-black font-semibold py-1 px-2 text-base 2xl:text-lg">Inicio</h1></Link>
-                    <Link to={"/about"} className="hover:bg-gray-300"><h1 className="text-black font-semibold py-1 px-2 text-base 2xl:text-lg">Sobre nosotros</h1></Link>
-                    <h1 className=" text-black font-semibold py-1 px-2 text-base 2xl:text-lg">Productos</h1>
-                    <div className='flex flex-col divide-y divide-gray-300 py-2'>
-                        <Link to={"/calefactores"} className="hover:bg-gray-300 ml-3"><h2 className="text-sm 2xl:text-base duration-200 px-2 py-1">Calefactores</h2></Link>
-                        <Link to={"/aires"} className="hover:bg-gray-300 ml-3"><h2 className="text-sm 2xl:text-base duration-200 px-2 py-1">Aires acondicionados</h2></Link>
-                        <Link to={"/todos"} className="hover:bg-gray-300 ml-3"><h2 className="text-sm 2xl:text-base duration-200 px-2 py-1">Todos los productos</h2></Link>
-                        <Link className="hover:bg-gray-300 ml-3"><h2 className="text-sm 2xl:text-base duration-200 px-2 py-1">Otros productos</h2></Link>
+                <article className="text-base 2xl:text-lg flex flex-col divide-y divide-gray-300">
+                    <Link to={"/"} className="hover:bg-gray-200 flex items-center pl-2"><img src={Home} className='h-5' alt='ícono de inicio' /><h1 className="text-black font-semibold py-1 px-2 text-base 2xl:text-lg">Inicio</h1></Link>
+                    <Link to={"/about"} className="hover:bg-gray-200 flex items-center pl-2"><img src={Info} className='h-5' alt='ícono de informacion' /><h1 className="text-black font-semibold py-1 px-2 text-base 2xl:text-lg">Sobre nosotros</h1></Link>
+                    <Link to={"/todos"} className="hover:bg-gray-200 flex items-center pl-2"><img src={Products} className='h-5' alt='ícono de productos' /><h1 className=" text-black font-semibold py-1 px-2 text-base 2xl:text-lg">Productos</h1></Link>
+                    <div className='flex flex-col divide-y divide-gray-200 py-2'>
+                        <Link to={"/calefactores"} className="hover:bg-gray-100 ml-3"><h2 className="text-sm 2xl:text-base duration-200 px-2 py-1">Calefactores</h2></Link>
+                        <Link to={"/aires"} className="hover:bg-gray-100 ml-3"><h2 className="text-sm 2xl:text-base duration-200 px-2 py-1">Aires acondicionados</h2></Link>
+                        <Link to={"/todos"} className="hover:bg-gray-100 ml-3"><h2 className="text-sm 2xl:text-base duration-200 px-2 py-1">Todos los productos</h2></Link>
+                        <Link className="hover:bg-gray-100 ml-3"><h2 className="text-sm 2xl:text-base duration-200 px-2 py-1">Otros productos</h2></Link>
                     </div>
                     {/* <h1 className="text-black font-semibold py-1 px-2 text-base 2xl:text-lg">Mantenimiento</h1>
                     <h1 className="text-black font-semibold py-1 px-2 text-base 2xl:text-lg">Otros servicios</h1> */}
                 </article>
                 <article className="flex flex-col gap-1 mt-4">
-                    <h1 className="font-semibold py-1 px-2 text-lg 2xl:text-xl">¿Dónde encontrarnos?</h1>
+                    <div className="flex items-center px-2 gap-1"><img src={Location} className='h-6' alt='ícono de ubicación' /><h1 className="font-semibold py-1 text-lg 2xl:text-xl">¿Dónde encontrarnos?</h1></div>
                     <a href="https://maps.app.goo.gl/38BwenRWHDBP9tkz7" target="_blank" rel="noopener noreferrer" className="rounded-xl border-2 border-gray-400 hover:cursor-pointer overflow-hidden"><img src={Map} alt='Mapa señalando la dirección del local: Tajes esquina Nardone, Santa Lucía, Canelones' className="hover:scale-110 duration-200" /></a>
+                    <h1 className='text-sm'>Tajes esq. Nardone, Santa Lucía, Canelones.</h1>
                 </article>
             </div>
         </aside>
--- src/pages/ProductCat.jsx
+++ src/pages/ProductCat.jsx
@@ -58,16 +58,16 @@ const ProductCat = () => {
                 <title>{title}</title>
                 <meta name="description" content={`Encuentre los mejores productos de ${title} a un precio increíble!`} />
             </Helmet>
-            <section className="py-4 flex flex-col gap-4">
+            <section className="py-4 flex flex-col gap-4 divide-y divide-gray-300">
                 <div className="flex flex-col 2xl:flex-row gap-2 2xl:items-end 2xl:gap-10">
                     <h1 className='text-2xl font-semibold lg:text-[2.2rem] text-blue-800'>{title}</h1>
-                    {(cat == "calefactores") && <div className="flex flex-wrap sm:gap-4 text-gray-600">
+                    {(cat == "calefactores") && <div className="flex flex-wrap gap-1 max-sm:text-sm sm:gap-4 text-gray-600">
                         <h2 className={`hover:cursor-pointer hover:text-gray-800 mx-2 ${subCat == "" ? "border-b-2 border-blue-600" : ""}`} onClick={() => {setSubCat("")}}>Todos los calefactores</h2>
                         <h2 className={`hover:cursor-pointer hover:text-gray-800 mx-2 ${subCat == "pellet" ? "border-b-2 border-blue-600" : ""}`} onClick={() => {setSubCat("pellet")}}>Calefactores a pellet</h2>
                         <h2 className={`hover:cursor-pointer hover:text-gray-800 mx-2 ${subCat == "leña" ? "border-b-2 border-blue-600" : ""}`} onClick={() => {setSubCat("leña")}}>Calefactores a leña</h2>
                     </div>}
                 </div>
-                <div className="producto">
+                <div className="producto pt-4">
                     {productsToPut.map(item => (
                         <DestCard key={item.idx + 1000} clase={"w-auto"} url={item.url} idx={item.idx} nombre={item.nombre} descripcion={item.descripcion} precio={item.precio} moneda={item.moneda} />
                     ))}
